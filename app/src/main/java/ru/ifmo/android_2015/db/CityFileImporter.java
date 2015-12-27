0466796a91d4eea0e3cb86452f9e6b4e19ecad7f
Anton Belyy <darya.nightmare@gmail.com>
2015-12-28 00:07:12 +0300

    added database optimizations

--- app/src/main/java/ru/ifmo/android_2015/db/CityFileImporter.java
+++ app/src/main/java/ru/ifmo/android_2015/db/CityFileImporter.java
@@ -2,6 +2,7 @@ package ru.ifmo.android_2015.db;
 
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -22,33 +23,25 @@ public abstract class CityFileImporter implements CityParserCallback {
 
     private SQLiteDatabase db;
     private int importedCount;
+    private SQLiteStatement statement;
 
     public CityFileImporter(SQLiteDatabase db) {
         this.db = db;
+        this.statement = db.compileStatement("INSERT INTO cities (_id, name, country, latitude, longitude) " +
+                "VALUES (?, ?, ?, ?, ?)");
     }
 
     public final synchronized void importCities(File srcFile,
                                                 ProgressCallback progressCallback)
             throws IOException {
 
-        InputStream in = null;
 
         try {
             long fileSize = srcFile.length();
-            in = new FileInputStream(srcFile);
-            in = new BufferedInputStream(in);
-            in = new ObservableInputStream(in, fileSize, progressCallback);
-            in = new GZIPInputStream(in);
-            importCities(in);
+            importCities(new GZIPInputStream(new ObservableInputStream(new BufferedInputStream(new FileInputStream(srcFile)), fileSize, progressCallback)));
 
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "Failed to close file: " + e, e);
-                }
-            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Failed to close file: " + e, e);
         }
     }
 
@@ -56,38 +49,45 @@ public abstract class CityFileImporter implements CityParserCallback {
 
     private void importCities(InputStream in) {
         CityJsonParser parser = createParser();
+        db.beginTransaction();
         try {
             parser.parseCities(in, this);
 
         } catch (Exception e) {
             Log.e(LOG_TAG, "Failed to parse cities: " + e, e);
+        } finally {
+            db.endTransaction();
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Failed to close SQLiteStatement: " + e, e);
+                }
+            }
         }
     }
 
     @Override
     public void onCityParsed(long id, String name, String country, double lat, double lon) {
-        insertCity(db, id, name, country, lat, lon);
+        insertCity(id, name, country, lat, lon);
         importedCount++;
         if (importedCount % 1000 == 0) {
             Log.d(LOG_TAG, "Processed " + importedCount + " cities");
         }
     }
 
-    private boolean insertCity(SQLiteDatabase db,
-                               long id,
+    private boolean insertCity(long id,
                                @NonNull String name,
                                @NonNull String country,
                                double latitude,
                                double longitude) {
-        final ContentValues values = new ContentValues();
-        values.put(CityContract.CityColumns.CITY_ID, id);
-        values.put(CityContract.CityColumns.NAME, name);
-        values.put(CityContract.CityColumns.COUNTRY, country);
-        values.put(CityContract.CityColumns.LATITUDE, latitude);
-        values.put(CityContract.CityColumns.LONGITUDE, longitude);
-
-        long rowId = db.insert(CityContract.Cities.TABLE, null /*nullColumnHack not needed*/, values);
-        if (rowId < 0) {
+        statement.bindLong(1, id);
+        statement.bindString(2, name);
+        statement.bindString(3, country);
+        statement.bindDouble(4, latitude);
+        statement.bindDouble(5, longitude);
+        long rodId = statement.executeInsert();
+        if (rodId < 0) {
             Log.w(LOG_TAG, "Failed to insert city: id=" + id + " name=" + name);
             return false;
         }
